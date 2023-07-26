b79295650cff453fdea3daaaec235c2428250cfa
lucascasagrande98 <lucascasagrande98@gmail.com>
2023-07-26 14:14:02 -0300

    projeto concluído

---
+++ src/components/List/index.tsx
@@ -0,0 +1,23 @@
+import style from "./List.module.scss";
+import Item from "./Item";
+import { ITarefa } from "../../types/tarefa";
+
+interface Props {
+	tarefas: ITarefa[];
+	selecionaTarefa: (tarefaSelecionada: ITarefa) => void;
+}
+
+function List({ tarefas, selecionaTarefa }: Props) {
+	return (
+		<aside className={style.listaTarefas}>
+			<h2>Estudos do dia</h2>
+			<ul>
+				{tarefas.map((item) => (
+					<Item selecionaTarefa={selecionaTarefa} key={item.id} {...item} />
+				))}
+			</ul>
+		</aside>
+	);
+}
+
+export default List;
---
+++ src/pages/App.tsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import Form from "../components/Form";
+import List from "../components/List";
+import Timer from "../components/Timer";
+import style from "./App.module.scss";
+import { ITarefa } from "../types/tarefa";
+
+function App() {
+	const [tarefas, setTarefas] = useState<ITarefa[]>([]);
+	const [selecionado, setSelecionado] = useState<ITarefa>();
+
+	function selecionaTarefa(tarefaSelecionada: ITarefa) {
+		setSelecionado(tarefaSelecionada);
+		setTarefas((tarefasAnteriores) =>
+			tarefasAnteriores.map((tarefa) => ({
+				...tarefa,
+				selecionado: tarefa.id === tarefaSelecionada.id ? true : false,
+			}))
+		);
+	}
+
+	function finalizarTarefa() {
+		if (selecionado) {
+      setSelecionado(undefined);
+			setTarefas((tarefasAnteriores) =>
+				tarefasAnteriores.map((tarefa) => {
+					if (tarefa.id === selecionado.id) {
+						return {
+							...tarefa,
+							selecionado: false,
+							completado: true,
+						};
+					}
+					return tarefa;
+				})
+			);
+		}
+	}
+
+	return (
+		<div className={style.AppStyle}>
+			<Form setTarefas={setTarefas} />
+			<List tarefas={tarefas} selecionaTarefa={selecionaTarefa} />
+			<Timer selecionado={selecionado} finalizarTarefa={finalizarTarefa} />
+		</div>
+	);
+}
+
+export default App;
